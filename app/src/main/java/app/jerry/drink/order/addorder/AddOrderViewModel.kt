78553afa86eb7299e24c8344b55966b47980a20a
jerrybo1014 <jerrybobobobo@gmail.com>
2020-02-10 14:00:24 +0800

    add add_order_fragment UI

---
+++ app/src/main/java/app/jerry/drink/order/addorder/AddOrderViewModel.kt
@@ -0,0 +1,12 @@
+package app.jerry.drink.order
+
+import androidx.lifecycle.ViewModel
+import app.jerry.drink.dataclass.OrderLists
+import app.jerry.drink.dataclass.source.DrinkRepository
+
+class AddOrderViewModel(private val repository: DrinkRepository, private val orderLists: OrderLists) : ViewModel() {
+
+
+
+
+}
